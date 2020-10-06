e45efa266346d6311eb2ccf8c8cca4bad37bdef7
weitsan <48983976+weitsan@users.noreply.github.com>
2020-10-06 23:42:04 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -22,8 +22,10 @@ let soundModel = './model/';
 let label = "開始收音";
 
 // Teachable Machine model URL:
-let soundModelURL = 'https://weitsan.github.io/Frog0908/model/model.json';
 
+let soundModelURL = 'https://city535353.github.io/frogdetect/model/model.json';
+//file:///D:/桌面/學校公務/108/108-2/智在家鄉/html/model/model.json
+//https://city535353.github.io/frogdetect/model/model.json  原網址
 
 function preload() {
   // Load the model
@@ -48,8 +50,10 @@ function draw() {
   
   
 }
-
-
+var countType = 0;
+var TimeCount = 0;
+var TimeID;
+var ani;
 // The model recognizing a sound will trigger this event
 function gotResult(error, results) {
   if (error) {
@@ -59,19 +63,46 @@ function gotResult(error, results) {
   // The results are in an array ordered by confidence.
   // console.log(results[0]); results[0].label
   if(results[0].label == 'mose') {
-	label = "諸羅樹蛙";
+    label = "莫式樹蛙";
   }else if(results[0].label == 'taipei'){
-	label = "澤蛙";
+	  label = "台北樹蛙";
   }else if(results[0].label == 'draw'){
-	label = "瓜瓜蛙" ;
+	  label = "諸羅樹蛙" ;
   }
-   
+
+  //如果辨識結果有東西
+  if(results[0].label != null){
+    TimeID = setInterval(Time(results[0].label),500);//每0.5秒進Time function，
+  }
   //label = results[0].label;
-  
+
 }
 
-
-
-
+function Time(targ){
+  //尚未到達5秒時
+  if(TimeCount%10 != 0 && TimeCount != 0){
+    TimeCount+=1;//訪問計數加1
+    if(ani == targ){
+      countType += 1;
+    }else{
+      ani = targ;//ani變數等於預測的結果
+    }
+    
+    if(TimeCount%10 == 0){//當訪問達5秒時
+      //傳送資料
+      recordurl = "http://210.240.203.166/test1/record.aspx?lid="+ lid +"&freq="+countType;
+      PostData();
+      //停止計時
+      clearInterval(TimeID);
+    }
+  }
+}
+function PostData(){
+    var requ = new XMLHttpRequest();
+    requ.open("GET",myurl);
+    requ.onload=function(){
+    };
+    requ.send();
+  }
 
 
